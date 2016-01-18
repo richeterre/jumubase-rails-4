356eb0ab5c8ecde220e73d56729db816de300f5e
Martin Richter <me@martinrichter.net>
2016-01-19 00:08:33 +0100

    Use t.references syntax in Contest migration and re-run all migrations

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160118224742) do
   create_table "contests", force: :cascade do |t|
     t.integer  "season",           null: false
     t.integer  "level",            null: false
-    t.integer  "host_id",          null: false
+    t.integer  "host_id"
     t.date     "begins",           null: false
     t.date     "ends",             null: false
     t.date     "certificate_date"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20160118224742) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
+
+  add_index "contests", ["host_id"], name: "index_contests_on_host_id", using: :btree
 
   create_table "hosts", force: :cascade do |t|
     t.string   "name",         null: false
@@ -73,5 +75,6 @@ ActiveRecord::Schema.define(version: 20160118224742) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "contests", "hosts"
   add_foreign_key "performances", "contests"
 end
