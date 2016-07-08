4c4a1638de69ff11c11ec2aab2b1540e7f65efc4
Martin Richter <me@martinrichter.net>
2016-07-09 00:58:56 +0200

    Add test for /contests endpoint

---
+++ spec/requests/api/v1/contests_spec.rb
@@ -0,0 +1,12 @@
+describe "Contests endpoint" do
+  it "returns all contests" do
+    FactoryGirl.create_list(:contest, 5)
+
+    get '/api/v1/contests'
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(json['contests'].length).to eq(5)
+  end
+end
