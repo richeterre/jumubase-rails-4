f897691485f75a861d5773ac7fcbab00867f96a6
Martin Richter <me@martinrichter.net>
2016-07-12 21:08:48 +0200

    Grant admins and inspectors access to all scoped performances without complicated filter query

--- app/policies/performance_policy.rb
+++ app/policies/performance_policy.rb
@@ -11,18 +11,22 @@ class PerformancePolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-      # Get the ids of all contest categories the user can access
-      contest_category_ids = ContestCategoryPolicy::Scope.new(user, ContestCategory).resolve.select("id")
+      if user.admin? || user.inspector?
+        scope
+      else
+        # Get the ids of all contest categories the user can access
+        contest_category_ids = ContestCategoryPolicy::Scope.new(user, ContestCategory).resolve.select("id")
 
-      # Get all performances associated with those contest categories, either directly or by predecessor
-      scope
-        .includes(:predecessor)
-        .where(
-          "performances.contest_category_id IN (?) OR predecessors_performances.contest_category_id IN (?)",
-          contest_category_ids,
-          contest_category_ids
-        )
-        .references(:predecessor)
+        # Get all performances associated with those contest categories, either directly or by predecessor
+        scope
+          .includes(:predecessor)
+          .where(
+            "performances.contest_category_id IN (?) OR predecessors_performances.contest_category_id IN (?)",
+            contest_category_ids,
+            contest_category_ids
+          )
+          .references(:predecessor)
+      end
     end
   end
 end
