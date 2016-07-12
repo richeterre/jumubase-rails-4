a2b5c87f749af4669d163ea17615be16be25bf68
Martin Richter <me@martinrichter.net>
2016-07-12 21:46:34 +0200

    Grant admins and inspectors access to all contest categories without resorting to regular users' filter query

--- app/policies/contest_category_policy.rb
+++ app/policies/contest_category_policy.rb
@@ -1,9 +1,13 @@
 class ContestCategoryPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      # Get the ids of all contests the user can access
-      contest_ids = ContestPolicy::Scope.new(user, Contest).resolve.select("id")
-      scope.where(contest_id: contest_ids)
+      if user.admin? || user.inspector?
+        scope
+      else
+        # Get the ids of all contests the user can access
+        contest_ids = ContestPolicy::Scope.new(user, Contest).resolve.select("id")
+        scope.where(contest_id: contest_ids)
+      end
     end
   end
 end
